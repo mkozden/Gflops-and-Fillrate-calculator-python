3f2fe466b52a91bfbc0d996fb8524904ab5a3707
godofpainTR <metekaanozden@gmail.com>
2016-08-23 19:35:42 +0300

    Combined core config input.
    Also added more output.

--- gflops and fillrate calculator.py
+++ gflops and fillrate calculator.py
@@ -1,7 +1,5 @@
 # Getting the input.
-shader = int(input('Enter the number of shader processors:'))
-tmp = int(input('Enter the amount of TMPs:'))
-rop = int(input('Enter the amount of ROPs:'))
+shader,tmp,rop = map(int,input('Enter the core config:').split(":"))
 core = int(input('Enter the core clock(in MHz):'))
 bit = int(input("Enter the bits of the GPU:"))
 mem = int(input("Enter the memory clock(in MHz):"))
@@ -11,6 +9,8 @@ gpixels = (core*rop/1000)
 gtexels = (core*tmp/1000)
 gbytes = (mem*2*bit/4/1000)
 #The results
+print ("The core config is: ",shader,":",tmp,":",rop)
+print ("The memory clock is: ",mem," MHz","  The core clock is: ",core," MHz")
 print (gflops ," GFLOPS")
 print (gpixels ," Gpixel/s")
 print (gtexels ," Gtexel/s")
