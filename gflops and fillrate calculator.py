a24479a3a6fe6b29346ad2e2101147907aaac1fa
root <root@DESKTOP-METE.localdomain>
2017-03-05 00:09:12 +0300

    Some small fixes here and there

--- gflops and fillrate calculator.py
+++ gflops and fillrate calculator.py
@@ -1,5 +1,6 @@
 from tabulate import tabulate
 from os import system as cmd
+import os , time
 while True:
     # Getting the input.
     shader1,tmp1,rop1 = map(int,input('Enter the core config:').split(":"))
@@ -23,7 +24,7 @@ while True:
     print ("Type 1 to start again, type 2 to compare this card with another one, or type 3 to quit.")
     choice = input()
     if choice == "1":
-        cmd("cls")
+        cmd("cls" if os.name == "nt" else "clear")
         continue
     elif choice == "2":
         shader2,tmp2,rop2 = map(int,input("Enter the core config (GPU 2):").split(":"))
@@ -36,7 +37,17 @@ while True:
         gbytes2 = (mem2*2*bit2/4/1000)
         table=[[" ","GPU1","GPU2","Difference(%)"],["GFLOPS",gflops1,gflops2,(gflops1-gflops2)/gflops2*100],["Gpixel/s",gpixels1,gpixels2,(gpixels1-gpixels2)/gpixels2*100],["Gtexel/s",gtexels1,gtexels2,(gtexels1-gtexels2)/gtexels2*100],["GB/s",gbytes1,gbytes2,(gbytes1-gbytes2)/gbytes2*100],["Average Diff.","-","-",(((gflops1-gflops2)/gflops2*100)+((gtexels1-gtexels2)/gtexels2*100)+((gpixels1-gpixels2)/gpixels2*100)+((gbytes1-gbytes2)/gbytes2*100))/4]]
         print(tabulate(table))
-        print(" ")
-        choice
+        time.sleep(3)
+        print("Press 1 to start again, type 2 to quit")
+        choice2 = input()
+        if choice2 == "1":
+            continue
+        elif choice2 == "2":
+            break
+        else:
+            print("Somethin's ain't right")
+            raise TypeError
+        break
     elif choice == "3":
         break
+        quit()
